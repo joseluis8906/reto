b2dce04d265ca08a6f86355d997c0186ecd5ab17
Jose Cáceres <joseluis8906@opmbx.org>
2017-06-01 14:37:32 -0500

    actualizar

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,19 +1,10 @@
 module.exports = {
   //funciones del controlador
   index: function(req, res, next) {
-<<<<<<< HEAD
-    res.render('login', 
-    {
-        message: req.flash('info'),
-        authmessage: req.flash('authmessage'),
-        verify: req.flash('verify')
-    });
-=======
     res.render('login', {
             message: req.flash('info'),
             authmessage: req.flash('authmessage'),
             verify: req.flash('verify')
         });
->>>>>>> 4befa4dd08d1aa5ee204cce460e5a61f5362a0ef
   }
 }
