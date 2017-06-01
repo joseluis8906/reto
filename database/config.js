6d50cc638d21bcb0247c2c20fe553379f2da53d1
Jose Cáceres <joseluis8906@opmbx.org>
2017-06-01 13:02:44 -0500

    actualizado

--- database/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    host: '127.0.0.1',
-    user: 'reto',
-    password: 'AHJ6L-9KD26-Y0BW4-QJT71-8CVAF',
-    dateString: true,
-    database: 'reto'
-};
-
-module.exports = config;
--- routes/routes.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('passport');
-var controllers = require('../controllers');
-//var AuthMiddleware = require('.././middleware/auth');
-
-router.get('/', controllers.HomeController.index);
