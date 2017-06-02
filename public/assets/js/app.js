243638f43bedc484e0dd052f8086a1b4895afcfe
Julio César <llulioscesar@gmail.com>
2017-06-01 22:17:55 -0500

    aa

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+    var svg = $("#cesar-mapa");
+    console.log(svg);
+
+
   if (navigator.geolocation) {
     navigator.geolocation.watchPosition(function(position) {
       mostrarUbicacion(position.coords);
@@ -77,10 +81,11 @@ $(document).ready(function() {
   });
 });
 
+
 ConsumoYa = {
 
   enviarPOST: function(url, parametro, valor, callBack){
-    var xhttp = new XXMLHttpRequest();
+    var xhttp = new XMLHttpRequest();
     xhttp.open("POST", url);
     xhttp.onreadystatechange = function(){
       if(this.readyState === 4 && this.status == 200){
@@ -89,44 +94,8 @@ ConsumoYa = {
     };
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(parametro + "=" + encodeURIComponent(JSON.stringify(valor)));
-  },
-
-  initVectorMap: function() {
-    var mapData = {
-      "AU": 760,
-      "BR": 550,
-      "CA": 120,
-      "DE": 1300,
-      "FR": 540,
-      "GB": 690,
-      "GE": 200,
-      "IN": 200,
-      "RO": 600,
-      "RU": 300,
-      "US": 2920,
-    };
-
-    $('#worldMap').vectorMap({
-      map: 'world_mill_en',
-      backgroundColor: "transparent",
-      zoomOnScroll: false,
-      regionStyle: {
-        initial: {
-          fill: '#e4e4e4',
-          "fill-opacity": 0.9,
-          stroke: 'none',
-          "stroke-width": 0,
-          "stroke-opacity": 0
-        }
-      },
-
-      series: {
-        regions: [{
-          values: mapData,
-          scale: ["#AAAAAA", "#444444"],
-          normalizeFunction: 'polynomial'
-        }]
-      },
-    });
   }
+
+
+
 }
